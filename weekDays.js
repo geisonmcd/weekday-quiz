4cc74352d02e52a0c112918e51c581cc5213de35
Geison <geisonmcd@gmail.com>
2022-04-04 16:06:54 -0300

    Update weekDays.js

--- weekDays.js
+++ weekDays.js
@@ -8,4 +8,17 @@ function findDayOfWeek(k, s) {
     return newArray[weekdays.indexOf(k) + s];
 }
 
-console.log(findDayOfWeek('sat', 33));
+const findDayOfWeek2 = (k, s) => {
+    const mod = s % 7
+    const toAdd = (weekdays.indexOf(k) + 1 + mod) % 7
+    return weekdays[toAdd-1]
+}
+
+const numberS = 100000000
+const weekDay = 'sat'
+console.time('Geison')
+console.log(findDayOfWeek(weekDay, numberS));
+console.timeEnd('Geison')
+console.time('Patrick')
+console.log(findDayOfWeek2(weekDay, numberS));
+console.timeEnd('Patrick')
